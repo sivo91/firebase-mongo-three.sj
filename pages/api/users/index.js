bd22c11113944fb5cb910077b1f69fc21d3fd59b
Peter <p.sivak91@gmail.com>
2022-11-18 10:45:32 -0800

    cors

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -29,14 +29,7 @@ connectDB()
       })
 export default handler */
 
- async (req, res) => {
-  const { method } = req;
 
-  // This will allow OPTIONS request
-  if (method === "OPTIONS") {
-    return res.status(200).send("ok");
-  }
-};
 
 
 export default async function handler(req, res) {
