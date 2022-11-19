248517e38ec2e658f4cf19b52bb72c1e14adff58
Peter <p.sivak91@gmail.com>
2022-11-18 21:23:14 -0800

    cors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 const { default: next } = require('next');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+/* const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   
 }
 
-module.exports = nextConfig
+module.exports = nextConfig */
+
+// next.config.js
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://firebase-mongo-three.vercel.app/:path*',
+          },
+        ]
+      },
+  };
