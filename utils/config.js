37c730d2c5de41691c538991c830b337f6a89dd8
Peter <p.sivak91@gmail.com>
2022-11-19 20:37:42 -0800

    cors

--- utils/config.js
+++ utils/config.js
@@ -3,15 +3,7 @@
 import axios from "axios";
 
 const api  = axios.create({
-  headers: {
-      "source": "/api/(.*)",
-      "headers": [
-        { "key": "Access-Control-Allow-Credentials", "value": "true" },
-        { "key": "Access-Control-Allow-Origin", "value": "*" },
-        { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-      ]
-    },
+  //headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   baseURL: "http://localhost:3000/api",
 });
 
