ad54cca07c4f6c43ab8f0955819e1332eeb537f3
Peter <p.sivak91@gmail.com>
2022-11-20 19:47:58 -0800

    cors

--- next.config.js
+++ next.config.js
@@ -9,26 +9,13 @@ const { default: next } = require('next');
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/api/(.*)",
         headers: [
-          /* {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          }, */
-          {
-            "source": "/api/(.*)",
-            "headers": [
               { "key": "Access-Control-Allow-Credentials", "value": "true" },
               { "key": "Access-Control-Allow-Origin", "value": "*" },
               { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
               { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
             ]
-          }
-        ]
       }
     ]
   }
